eb98745ee8231cf5d6dd102600c45de9da9a4cca
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2019-05-26 05:08:19 -0700

    added exercise 1

---
+++ references/chapter1/exercises/ex1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+print("hello world!")
+print("hello again!")
+print("I like typing this.")
+print("This is fun.")
+print('yay! printing.')
+print("I'd much rather you 'not'")
+print('I "said" do not touch this.')
