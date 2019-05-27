cebd2761f294e6d18fbb5aa8941a8b4faf865244
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2019-05-26 17:38:42 -0700

    added exercise 10

---
+++ references/exercises/ex10.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "\tI'm split\n on a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backlash_cat)
+print(fat_cat)
